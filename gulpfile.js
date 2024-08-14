8bfe6f6cb79fe8af72513d7ab2b01227e4817ada
Davi <mdavi6085@gmail.com>
2024-08-13 22:26:02 -0300

    adicionando o hero

--- gulpfile.js
+++ gulpfile.js
@@ -11,18 +11,20 @@ function scripts(){
 
 function styles(){
     return gulp.src('./src/styles/*.scss')
-        .pipe(sass( {outputStyle: 'compressed'} ))
+        .pipe(sass({ outputStyle: 'compressed' }))
         .pipe(gulp.dest('./dist/css'));
 }
 
 function images(){
-    return gulp.src('./src/image/**/*')
+    return gulp.src('./src/images/**/*')
         .pipe(imagemin())
-        .pipe(gulp.dest('./dist/image'));
+        .pipe(gulp.dest('./dist/images'));
 }
 
 exports.default = gulp.parallel(styles, images, scripts);
+
 exports.watch = function(){
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
     gulp.watch('./src/scripts/*.js', gulp.parallel(scripts));
-}
+    gulp.watch('./src/images/**/*', gulp.parallel(images)); // Monitorar alterações nas imagens
+};
