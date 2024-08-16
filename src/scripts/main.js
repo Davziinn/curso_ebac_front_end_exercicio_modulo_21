0fc99bcb00a43a184d74f570ab1389684ad9cbb1
Davi <mdavi6085@gmail.com>
2024-08-16 20:04:50 -0300

    adicionando o projeto para desktop completo

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,23 +1,49 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener('DOMContentLoaded', function () {
+    const questions = document.querySelectorAll('.faq__list__item__question');
+
+    questions.forEach(function (question) {
+        question.addEventListener('click', function () {
+            const listItem = question.parentElement;
+            const isOpen = listItem.classList.contains('faq__list__item--is-open');
+
+            document.querySelectorAll('.faq__list__item--is-open').forEach(function (item) {
+                item.classList.remove('faq__list__item--is-open');
+            });
+
+            if (!isOpen) {
+                listItem.classList.add('faq__list__item--is-open');
+            }
+        });
+    });
+
     const tabs = document.querySelectorAll('.shows__tabs__button');
     const lists = document.querySelectorAll('.shows__list');
 
-    // Iterando sobre os botões com um loop for
     for (let i = 0; i < tabs.length; i++) {
         tabs[i].addEventListener('click', function () {
-            // Remover a classe ativa de todos os botões e listas
             for (let j = 0; j < tabs.length; j++) {
                 tabs[j].classList.remove('shows__tabs__button--is-active');
                 lists[j].classList.remove('shows__list--is-active');
             }
 
-            // Adicionar a classe ativa ao botão clicado e à lista correspondente
             tabs[i].classList.add('shows__tabs__button--is-active');
             lists[i].classList.add('shows__list--is-active');
         });
     }
 
-    // Ativar a primeira aba e lista por padrão
     tabs[0].classList.add('shows__tabs__button--is-active');
     lists[0].classList.add('shows__list--is-active');
+
+    const header = document.querySelector('.header');
+    const sticky = header.offsetTop;
+
+    function stickyHeader() {
+        if (window.pageYOffset > sticky) {
+            header.classList.add('header--sticky');
+        } else {
+            header.classList.remove('header--sticky');
+        }
+    }
+
+    window.addEventListener('scroll', stickyHeader);
 });
